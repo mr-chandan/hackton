6bc1f8ccc0b268f20626ae4c636e8a2703690904
chandan <chandanchethan321@gmail.com>
2023-02-05 10:46:37 +0530

    vv

--- Backend/controllers/Stepone.js
+++ Backend/controllers/Stepone.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+const mysqlConnection = require('../database/Sqlconnect')
+
 
 class Stepone {
     async search(req, res) {
@@ -14,7 +16,47 @@ class Stepone {
     }
 
     async store(req, res) {
-        quearylist += `CREATE TABLE ${storedata}_${formData}_languages ( id int NOT NULL auto_increment,languages varchar(50) NOT NULL unique,PRIMARY KEY (id));`
+        const{email,nameofapp,whereufound,whatisitrealted,scamrelated} = req.body
+        var quearylist = `INSERT INTO review (email,nameofapp,whereufound,whatisitrealted,scamrelated)VALUES ("${email}","${nameofapp}","${whereufound}","${whatisitrealted}","${scamrelated}");`
+        const quearylist1 = `select * from review where nameofapp = "${nameofapp}";`
+        console.log(quearylist)
+        try {
+            mysqlConnection.query(quearylist1, function (err, results, rows) {
+                if (!err) {
+                        mysqlConnection.query(quearylist, (err, rows) => {
+                            if (!err) {
+                                res.status(200).send(rows);
+                            } else {
+                                res.status(502).send("Db error");
+                            }
+                        })
+                } else {
+                    res.status(502).send("Db error");
+                    console.log(err)
+                    console.log(results)
+                }
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async que(req, res) {
+        const quearylist1 = `select * from review;`
+        try {
+            mysqlConnection.query(quearylist1, function (err, results, rows) {
+                if (!err) {
+                    // console.log(results)
+                    res.status(200).send(results);
+                } else {
+                    res.status(502).send("Db error");
+                    console.log(err)
+                    console.log(results)
+                }
+            });
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
--- Backend/routes.js
+++ Backend/routes.js
@@ -3,5 +3,6 @@ const Stepone = require("./controllers/Stepone")
 
 router.post('/apilink', Stepone.search)
 router.post('/apilink/store', Stepone.store)
+router.post('/apilink/que', Stepone.que)
 
 module.exports = router;
